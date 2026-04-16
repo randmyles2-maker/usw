47e3d4644f75480e264f16eb89a4f42e38c03c8f
f1ways093 <randmyles2@gmail.com>
2026-04-16 12:43:39 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const USW_CONFIG = {
+    APP_NAME: "USW Network Secure Node",
+    VERSION: "2.0.0",
+    DEFAULT_PYTHON: "print('USW Network: Python Kernel Active')\nprint('Status: Secure')",
+    DEFAULT_JS: "console.log('USW Network: JavaScript Kernel Active');",
+    GITHUB: {
+        USER: "randmyles2-maker",
+        REPO: "usw"
+    }
+};
