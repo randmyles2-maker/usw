463e98812242d0d27b20dd75ecd45de587974bb7
f1ways093 <randmyles2@gmail.com>
2026-04-16 20:54:15 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,33 @@
-const CACHE_NAME = 'aether-v3';
+const CACHE_NAME = 'aether-v4';
 const ASSETS = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './storage.js',
-  './manifest.json',
-  './icon-512.png'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './storage.js',
+    './manifest.json',
+    './icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
+    );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
+    e.respondWith(
+        fetch(e.request).catch(() => caches.match(e.request))
+    );
 });
