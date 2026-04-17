50dce62734fee6c9dacbf90a269cbfcad5cea9a8
f1ways093 <randmyles2@gmail.com>
2026-04-16 20:12:44 -0400

    Update storage.js

--- storage.js
+++ storage.js
@@ -1,26 +1,26 @@
 const USW_DATA = {
     saveUser: (user, pass) => {
-        let users = JSON.parse(localStorage.getItem('usw_users') || '{}');
-        if (users[user]) return false;
-        users[user] = { pass: pass, snippets: {} };
-        localStorage.setItem('usw_users', JSON.stringify(users));
+        let u = JSON.parse(localStorage.getItem('usw_users') || '{}');
+        if (u[user]) return false;
+        u[user] = { pass: pass, snippets: {} };
+        localStorage.setItem('usw_users', JSON.stringify(u));
         return true;
     },
     verifyUser: (user, pass) => {
-        let users = JSON.parse(localStorage.getItem('usw_users') || '{}');
-        return users[user] && users[user].pass === pass;
+        let u = JSON.parse(localStorage.getItem('usw_users') || '{}');
+        return u[user] && u[user].pass === pass;
     },
     saveCode: (user, lang, code) => {
-        let users = JSON.parse(localStorage.getItem('usw_users') || '{}');
-        if (users[user]) {
-            users[user].snippets[lang] = code;
-            localStorage.setItem('usw_users', JSON.stringify(users));
+        let u = JSON.parse(localStorage.getItem('usw_users') || '{}');
+        if (u[user]) {
+            u[user].snippets[lang] = code;
+            localStorage.setItem('usw_users', JSON.stringify(u));
             return true;
         }
         return false;
     },
     loadCode: (user, lang) => {
-        let users = JSON.parse(localStorage.getItem('usw_users') || '{}');
-        return users[user]?.snippets[lang] || null;
+        let u = JSON.parse(localStorage.getItem('usw_users') || '{}');
+        return u[user]?.snippets[lang] || null;
     }
 };
